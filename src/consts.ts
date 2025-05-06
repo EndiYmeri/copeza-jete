0994c8e7bd96d1336de004dc1cbe6bcf881ac17f
Endi <56412395+EndiYmeri@users.noreply.github.com>
2025-05-06 10:44:42 +0200

    Init

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Copëza jete Photography";
+export const SITE_DESCRIPTION = "Capturing pieces of life through the lens.";
